62988c989141972689904f61a7e5268d2f66eea1
Your Name <test@example.com>
2020-11-27 15:24:15 +0100

    img upgrade

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="youtube.py",
-    version="1.0.0",
+    version="1.0.2",
     description="YouTube.py is a light python module for youtube.",
     long_description=README,
     long_description_content_type="text/markdown",
