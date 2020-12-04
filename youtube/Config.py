6b81ea98306298e6b501a70b7c647434c4b0b1ec
Your Name <test@example.com>
2020-12-04 10:06:56 +0100

    beta version

--- youtube/Config.py
+++ youtube/Config.py
@@ -3,6 +3,7 @@
 youtube.py is really lite python liberary with fast downloader
 """
 
+
 __title__ = "youtube.py"
 __version__ = "1.0.3"
 __author__ = "Mayank Gupta"
@@ -11,3 +12,4 @@ __copyright__ = "Copyright 2020 YouTube.py (Mayank Gupta)"
 __github__ = "https://github.com/youtube-py/youtube.py"
 __issues__ = "https://github.com/youtube-py/youtube.py/issues"
 __docs__ = "https://youtube-python.mayankfawkes.xyz/"
+
