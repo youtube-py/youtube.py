45ac3f471706752a35596d902c4bbfd8e9d49519
Mayank Gupta <mkgupta74d@gmail.com>
2021-05-10 06:41:52 +0200

    push and release v2.0.0

--- examples/test.py
+++ examples/test.py
@@ -3,14 +3,17 @@ import sys
 sys.path.insert(0,'..')
 
 import youtube as youtube
-from youtube import Search
+from youtube import Search, Video
 
-r = Search("a kermit song")
+streams = Video("https://www.youtube.com/watch?v=B6_iQvaIjXw").streams
+for n in streams:
+	print(n)
+	print(n.url)
+	print("\n\n")
+print(streams.ffresolution(720).av)
 
-print(r.get_dict)
-
-y = youtube.Video("https://www.youtube.com/watch?v=5sFDOiGyAG8")
-print(y)
+# y = youtube.Video("https://www.youtube.com/watch?v=5sFDOiGyAG8")
+# print(y)
 
 if __name__ == '__main__':
 	print(youtube.__version__)
---
+++ release.md
@@ -0,0 +1,10 @@
+
+# Changlogs
+
+## v2.0.0
+
+* bug fix
+* now ``search`` object is iterable
+* ``--connections`` added in cli to increase or decrease the no. of connections in download
+* downloading socket timeout increase to ``15`` seconds 
+* added ``GoogleReCaptchaError`` in exceptions
